5015a756ae86967900359377ee25d0d12e3ca422
Facundo Pucheta <facundopucheta94@gmail.com>
2023-09-27 20:16:58 -0300

    feature(/src/server.js): new server file created/config

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const server = express();
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
+server.use(morgan("dev"));
+
+// cors defined
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
+// acces to main router
+server.use()
+
+module.exports = server;    
