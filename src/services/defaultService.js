8d576fcd0249db6218ee48d98a7503e4671b1443
Facundo Pucheta <facundopucheta94@gmail.com>
2023-09-28 11:54:58 -0300

    feature: new utils implemented

--- src/services/defaultService.js
+++ src/services/defaultService.js
@@ -1,7 +1,9 @@
+const { ServerError } = require("../utils/errors");
+
 const defaultMsg = "Hola, funciona";
 
 const sendMessageDefault = async () => {
-  if (!defaultMsg) throw new Error("AHHHHHH ERROR");
+  if (!defaultMsg) throw new ServerError("No message created", 501);
   return defaultMsg;
 };
 
---
+++ src/utils/index.js
@@ -0,0 +1,7 @@
+// export utils
+module.exports = {
+  response: require("./response"),
+  errors: require("./errors"),
+  resError: require("./resError"),
+  catchedAsync: require("./catchedAsync"),
+};
